439b552cd72a9f43bb818cd4d3485d3b4326280d
Gosha <djabarov.george@gmail.com>
2020-04-22 19:35:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Map, TileLayer } from 'react-leaflet';
+import Search from "react-leaflet-search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  israel = {
+    telAviv: {
+      lat: 32.0853,
+      lng: 34.7818,
+    },
+    zoom: 13
+  }
+
+  render(){
+    const positionTelAviv = [this.israel.telAviv.lat, this.israel.telAviv.lng];
+    return (
+      <Map className="map" center={positionTelAviv} zoom={this.israel.zoom}>
+        <Search     provider="OpenStreetMap"
+                    position="topleft"
+                    zoom={14}
+                    inputPlaceholder="חפש מיקום"/>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+    );
+  }
 }
 
 export default App;
